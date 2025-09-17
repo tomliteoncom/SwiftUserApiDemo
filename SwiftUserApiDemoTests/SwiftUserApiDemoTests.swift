02d9cdc7785e684de7461d47c8b7777b434031ef
C-Tom Chen <c-tom.chen@liteon.com>
2025-09-17 16:23:01 +0800

    Initial Commit

---
+++ SwiftUserApiDemoTests/SwiftUserApiDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUserApiDemoTests.swift
+//  SwiftUserApiDemoTests
+//
+//  Created by C-Tom Chen on 2025/9/17.
+//
+
+import Testing
+@testable import SwiftUserApiDemo
+
+struct SwiftUserApiDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
